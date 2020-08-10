16180093440b391edba1e75d1b0b15d3b32c6ba9
Renato  Avellar <onizuka@Mac-mini.local>
2020-08-10 16:27:54 -0300

    criando arquivo de rotas

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import express from 'express';
+import routes from '../src/Routes/routes';
+
 
 const app = express();
 
 app.use(express.json());
+app.use(routes);
 
 app.listen(3333, ()=> console.log('🔥 Server started at http://localhost:3333 '));
