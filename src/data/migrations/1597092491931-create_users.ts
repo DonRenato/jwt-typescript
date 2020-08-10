cc7448efa1f504929ef32e4ba0b6f84e94702db7
Renato  Avellar <onizuka@Mac-mini.local>
2020-08-10 18:07:43 -0300

    criando tabela users

--- src/data/migrations/1597092491931-create_users.ts
+++ src/data/migrations/1597092491931-create_users.ts
@@ -1,13 +1,38 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+
 
 export class CreateUsers1597092491931 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"');
 
+        await queryRunner.createTable(new Table({
+            name: 'users',
+            columns: [
+                {
+                    name: 'id',
+                    type: 'uuid',
+                    isPrimary: true,
+                    generationStrategy: 'uuid',
+                    default: 'uuid_generate_v4()',
+                },
+                {
+                    name: 'email',
+                    type: 'varchar',
+                    isUnique: true,
+                },
+                {
+                    name: 'password',
+                    type: 'varchar',
+                },
+
+            ]
+        }));
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-
+        await queryRunner.dropTable('users');
+        await queryRunner.query('DROP_EXTENSION "uuid-ossp"');
     }
 
 }
