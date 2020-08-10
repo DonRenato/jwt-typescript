ead530925654df55d6e6a3e4a947dc3a6faaf798
Renato  Avellar <onizuka@Mac-mini.local>
2020-08-10 17:51:19 -0300

    renomeando migration

--- src/data/migrations/1597092491931-create_users.ts
+++ src/data/migrations/1597092491931-create_users.ts
@@ -1,11 +1,13 @@
 import {MigrationInterface, QueryRunner} from "typeorm";
 
-export class 00CreateUsers1597092491931 implements MigrationInterface {
+export class CreateUsers1597092491931 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
+
     }
 
 }
