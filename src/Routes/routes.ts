7496d54ffd0062861ed40fbaf2e02c827bb6259e
Renato  Avellar <onizuka@Mac-mini.local>
2020-08-10 23:55:16 -0300

    autenticando usuario

--- src/Routes/routes.ts
+++ src/Routes/routes.ts
@@ -1,11 +1,13 @@
 import Router from 'express';
 import UserController from '../app/controllers/UserController';
 import AuthController from '../app/controllers/AuthController';
+import AuthMiddleware from '../app/middlewares/authMiddleware';
 
 const routes = Router();
 
 routes.post('/users', UserController.store);
 routes.post('/auth', AuthController.authenticate);
+routes.get('/users', AuthMiddleware, UserController.index);
 
 
 export default routes;
--- src/app/controllers/UserController.ts
+++ src/app/controllers/UserController.ts
@@ -7,6 +7,12 @@ import User  from '../models/User';
 
 
 class UserController{
+    index(req: Request,res: Response){
+        return res.send({ userId: req.userId});
+    }
+
+
+
     async store(req: Request, res: Response){
         const repository = getRepository(User);
         const { email, password } = req.body;
